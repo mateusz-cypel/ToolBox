6ca446dbb1af8a481679b001c3491542d3444b9c
Mateusz Cypel <mateusz.cypel@stxnext.pl>
2024-05-28 10:35:24 +0200

    basic config

--- src/app.py
+++ src/app.py
@@ -1,8 +1,43 @@
-class App:
-    @staticmethod
-    def start():
-        print("Hello Word")
+from kivy.app import App
+from kivy.uix.label import Label
+
+WINDOW_TITLE = "ToolBox"
+WINDOW_WIDTH, WINDOW_HEIGHT = (600, 800)
+
+default_settings = (
+    # (section, key-values)
+    (
+        "window",
+        {
+            "width": WINDOW_WIDTH,
+            "height": WINDOW_HEIGHT,
+            "title": WINDOW_TITLE
+        }
+    ),
+)
+
+
+class ToolBoxApp(App):
+    use_kivy_settings = False
+
+    def build_config(self, config):
+        for section, key_values in default_settings:
+            config.setdefaults(section, key_values)
+
+    def build(self):
+        return Label(text="Hello World")
+
+    def on_start(self):
+        width = self.config.getint("window", "width")
+        height = self.config.getint("window", "height")
+        title = self.config.get("window", "title")
+
+        self.root_window.size = (width, height)
+        self.root_window.set_title(title)
+
+    def open_settings(self, *largs):
+        pass
 
 
 if __name__ == '__main__':
-    App.start()
+    ToolBoxApp().run()
