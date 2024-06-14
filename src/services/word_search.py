6eabcd4b95a73b7608ed4983ae5ce81ecab1270b
Mateusz Cypel <mateusz.cypel@stxnext.pl>
2024-06-14 23:23:09 +0200

    word checker for Polish words

---
+++ src/services/word_search.py
@@ -0,0 +1,90 @@
+from urllib.error import HTTPError
+from urllib.parse import quote, urljoin
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+SJP_URL = "https://sjp.pl"
+
+
+class SJPWordSearch:
+    class TagName:
+        H1 = "h1"
+        P = "p"
+
+    class AvailabilityTagText:
+        ACCEPTABLE = "dopuszczalne w grach (i)"
+        UNACCEPTABLE = "niedopuszczalne w grach (i)"
+        DOES_NOT_EXIST = "nie występuje w słowniku"
+
+        ALL = (
+            ACCEPTABLE,
+            UNACCEPTABLE,
+            DOES_NOT_EXIST,
+        )
+
+    class TagStyles:
+        BaseWordTag = "margin-bottom: 0;"
+        DefinitionTag = "margin: .5em 0; font: medium/1.4 sans-serif; max-width: 34em; "
+
+    class StopIterationTagText:
+        RELATES_WORDS = "powiązane hasła:"
+        COMMENTS = "komentarze"
+
+        ALL = (
+            RELATES_WORDS,
+            COMMENTS,
+        )
+
+    def __init__(self, base_url):
+        self._base_url = base_url
+
+    def search(self, word):
+        url = self._get_word_url(word)
+
+        try:
+            page = urlopen(url)
+        except HTTPError as e:
+            if e.code == 404:
+                return word, self.AvailabilityTagText.DOES_NOT_EXIST, "-", "-"
+            raise Exception
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+
+        tags = soup.body.contents
+
+        header_tags, availability_tags, base_word_tags, definition_tags = [], [], [], []
+
+        for tag in tags:
+            name = tag.name
+            text = tag.text.strip()
+
+            if name == self.TagName.H1:
+                header_tags.append(tag.text.strip().rstrip(" ✕"))
+            elif name == self.TagName.P:
+                if text.lower() in self.StopIterationTagText.ALL:
+                    break
+
+                if text.lower() in [self.AvailabilityTagText.DOES_NOT_EXIST]:
+                    availability_tags.append(tag.text.strip())
+                    base_word_tags.append("-")
+                    definition_tags.append("-")
+                    break
+
+                if text.lower() in [self.AvailabilityTagText.ACCEPTABLE, self.AvailabilityTagText.UNACCEPTABLE]:
+                    availability_tags.append(tag.text.strip())
+                elif tag.attrs['style'] == self.TagStyles.BaseWordTag:
+                    base_word_tags.append(tag.text.strip())
+                elif tag.attrs['style'] == self.TagStyles.DefinitionTag:
+                    definition_tags.append(tag.text.strip())
+
+        return [x for x in zip(header_tags, availability_tags, base_word_tags, definition_tags)]
+
+    def _get_word_url(self, word):
+        word = quote(word)
+        return urljoin(self._base_url, word)
+
+
+sjp_word_search = SJPWordSearch(
+    base_url=SJP_URL,
+)
--- src/views/main_panel.py
+++ src/views/main_panel.py
@@ -10,4 +10,32 @@ class ToolBoxMainPanel(TabbedPanel):
         self.qr_generator_tab.add_widget(qr_generator_controller.view)
         self.add_widget(self.qr_generator_tab)
 
+        from kivy.uix.button import Button
+        from kivy.uix.gridlayout import GridLayout
+        from kivy.uix.label import Label
+        from kivy.uix.textinput import TextInput
+
+        layout = GridLayout(rows=3)
+
+        btn = Button(text="TEST")
+        btn.bind(on_press=self.callback)
+
+        self.text_input = TextInput(multiline=False)
+        self.label = Label(text="")
+
+        layout.add_widget(self.text_input)
+        layout.add_widget(btn)
+        layout.add_widget(self.label)
+
+        self.word_checker_tab = TabbedPanelItem(text="Word checker")
+        self.word_checker_tab.add_widget(layout)
+        self.add_widget(self.word_checker_tab)
+
         self.set_def_tab(self.qr_generator_tab)
+
+    def callback(self, instance):
+        from services.word_search import sjp_word_search
+        print(sjp_word_search.search(self.text_input.text))
+
+
+
